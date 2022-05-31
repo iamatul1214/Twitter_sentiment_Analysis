fac75b5c618fddc70011ef888c6e84a4974f9d6b
atulkumarrai <iamatul1214@gmail.com>
2022-05-31 22:28:22 +0530

    base model creation done

--- src/stage_04_base_model_creation.py
+++ src/stage_04_base_model_creation.py
@@ -4,7 +4,10 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
+import tensorflow as tf
 import random
+import numpy as np
+from src.utils.common import callbacks
 from src.utils.data_preprocessing import keep_important_columns, replace_target_values,plot_data_distribution,separating_label_feature,train_test_split_operation,store_preprocessed_dataset,convert_data_into_numpy, convert_numpy_dataset_to_tensors
 from src.utils.data_validation import read_csv, check_null_values,check_binary_classification,check_data_distribution
 STAGE = "Data Validation"   ## Name of the stage
@@ -30,9 +33,17 @@ def main(config_path):
     train_test_split_ratio = config['model_config']['train_test_split']
     random_state = config['model_config']['random_state']
     shuffle = config['model_config']['shuffle']
-    
+    vocab_size = config['model_config']['VOCAB_SIZE']
+    batch_size = config['model_config']['BATCH_SIZE']
+    base_model_dir = config['artifacts']['BASE_MODEL_DIR']
+    base_model_name = config['artifacts']['BASE_MODEL_NAME']
+    output_dim = config['model_config']['OUTPUT_DIM']
+    tnsbrd_log_dir = config['artifacts']['TENSORBOARD_ROOT_LOG_DIR']
+    ckpt_dir = config['artifacts']['CHECKPOINT_DIR']
 
-    
+
+
+
     ## reading the dataframe
     dataframe = read_csv(file_location=file_location,file_name=file_name,columns=columns,encoding_type=encoding_type)
 
@@ -48,11 +59,6 @@ def main(config_path):
                                                          random_state =random_state, shuffle = shuffle)
 
 
-
-
-
-
-
     ## read the train test data from preprocessed dataset folder into to_csv
  #   x_train,x_test,y_train,y_test = read_train_test_split_datasets(file_location=preprocessed_dataset_folder,x_train = "x_train",x_test = "x_test",y_train = "y_train",y_test = "y_test")
     
@@ -64,6 +70,52 @@ def main(config_path):
     x_train_tensor,x_test_tensor,y_train_tensor,y_test_tensor = convert_numpy_dataset_to_tensors(x_train = x_train_numpy,y_train = y_train_numpy,
                                                                                                     x_test = x_test_numpy,y_test = y_test_numpy)
     logging.info(f"The shape of x_train_tensor = {len(x_train_tensor)}, y_train_tensor = {len(y_train_tensor)}")
+
+    ## creating encoder 
+    encoder = tf.keras.layers.TextVectorization(max_tokens=vocab_size)  ## textvectorization from tensorflow
+    logging.info(f"Encoder created")
+    encoder.adapt(x_train_tensor.batch(batch_size))
+    logging.info(f" Finished training the encoder on the x_train")
+
+    vocab = np.array(encoder.get_vocabulary())  # to create vocabulary
+    logging.info(f"The first 50 vocab = {vocab[:50]}")
+
+    ## creating embedding layer
+    embedding_layer = tf.keras.layers.Embedding(
+    input_dim = len(encoder.get_vocabulary()), # 1000
+    output_dim = output_dim, # 64
+    mask_zero = True
+) 
+    logging.info("creating embedding layer")
+
+
+    ## creating base model directory to store the base model
+    create_directories([base_model_dir])
+    
+    ## creating base model and saving it into the model directory
+    Layers = [
+          encoder, # text vectorization
+          embedding_layer, # embedding
+          tf.keras.layers.Bidirectional(
+              tf.keras.layers.LSTM(64)
+          ),
+          tf.keras.layers.Dense(64, activation="relu"),
+          tf.keras.layers.Dense(1)
+            ]
+
+    model = tf.keras.Sequential(Layers)
+    logging.info(f"Model summary looks like: {model.summary}")
+
+    model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), # logits means rw output without activation function
+    optimizer=tf.keras.optimizers.Adam(1e-4),
+    metrics=["accuracy"]
+)
+    model.save(os.path.join(base_model_dir,base_model_name))
+    logging.info(f"Saved the model into location: {base_model_dir}")
+
+    ## creating dirs for saving checkpoints and Logs
+    callbacks(tensorboard_log_dir=tnsbrd_log_dir, checkpoint_dir=ckpt_dir)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
--- src/utils/common.py
+++ src/utils/common.py
@@ -5,6 +5,7 @@ import time
 import pandas as pd
 import json
 from zipfile import ZipFile
+import tensorflow as tf
 
 
 def read_yaml(path_to_yaml: str) -> dict:
@@ -42,4 +43,29 @@ def rename_file(file_path:str,oldfile_name:str, newfile_name:str)->None:
             logging.info(f"Renamed the file from {oldfile_name} to {newfile_name}")
     except exception as e:
         logging.exception(e)
+
+def callbacks(tensorboard_log_dir, checkpoint_dir):
+
+  # tensorboard callbacks - 
+  unique_log = time.asctime().replace(" ", "_").replace(":", "")
+  tensorboard_log_dir = os.path.join(tensorboard_log_dir, unique_log)
+  os.makedirs(tensorboard_log_dir, exist_ok=True)
+
+  tb_cb = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_log_dir)
+
+  # ckpt callback
+  ckpt_file = os.path.join(checkpoint_dir, "model")
+  os.makedirs(checkpoint_dir, exist_ok=True)
+
+  ckpt_cb = tf.keras.callbacks.ModelCheckpoint(
+      filepath=ckpt_file,
+      save_best_only=True
+  )
+
+  callback_list = [
+                   tb_cb,
+                   ckpt_cb
+  ]
+
+  return callback_list
 
