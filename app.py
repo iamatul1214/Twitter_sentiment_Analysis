adf22e5b34de2c7becd96b1f08b5aab369ef094e
atulkumarrai <iamatul1214@gmail.com>
2022-06-05 20:07:14 +0530

    flask app created

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import yaml
+import joblib
+import numpy as np
+import tensorflow as tf
+
+
+webapp_root="webapp"
+config_file_path = "configs/config.yaml"
+
+static_dir=os.path.join(webapp_root,"static")
+template_dir=os.path.join(webapp_root,"templates")
+
+app=Flask(__name__,static_folder=static_dir, template_folder=template_dir)
+
+def read_yaml(config_path=config_file_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predict(text):
+    config = read_yaml(config_file_path)
+    ckpt_dir = config['artifacts']['CHECKPOINT_DIR']
+    trained_model_dir = config['artifacts']['TRAINED_MODEL_DIR']
+    path_to_trained_model = os.path.join(ckpt_dir,trained_model_dir)
+    trained_model = tf.keras.models.load_model(path_to_trained_model)
+    predictions = trained_model.predict(np.array([text]))
+    score = predictions[0][0]
+    if score > 0:
+        return "positive sentiment"
+    else:
+        return "negative sentiment"
+    
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method=='POST':
+       try:
+           if request.form:               
+               tweet = request.form.get('TWEET')
+               response = predict(np.array([tweet]))
+
+               return render_template('index.html',response=response)
+
+       except Exception as e:
+           print(e)
+           # error={"error":"Something went wrong!! Try again"}
+           error={"error":e}
+           return render_template("404.html", error=error)
+    else:
+        return render_template('index.html')
+        
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
